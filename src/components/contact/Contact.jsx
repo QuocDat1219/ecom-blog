a82311f160117ccf5470a0f99a3e9d7d8ebd6c9d
thuan <lapthuan0805@gmail.com>
2023-03-23 18:28:20 +0700

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,8 +10,8 @@ const Contact = () => {
         <div className="containers">
           <form className="shadow">
             <h4>Tìm kiếm cửa hàng</h4> <br />
-            <div class="row">
-              <div class="col-sm">
+            <div class="flex flex-wrap ">
+              <div class="relative sm:flex-grow sm:flex-1">
                 <select class="browser-default custom-select">
                   <option selected>Chọn tỉnh, thành phố</option>
                   <option value="1">Cần Thơ</option>
@@ -19,16 +19,16 @@ const Contact = () => {
                   <option value="3">Bình Dương</option>
                 </select>
               </div>
-              <div class="col-sm">
+              <div class="relative sm:flex-grow sm:flex-1">
                 <select class="browser-default custom-select">
                   <option selected>Chọn quận, huyện</option>
                 </select>
               </div>
-              <div class="col-sm">
+              <div class="relative sm:flex-grow sm:flex-1">
                 <button class="btntk">Tìm kiếm</button>
               </div>
             </div>
-            <div class="row" style={{ paddingTop: "20px" }}>
+            <div class="flex flex-wrap " style={{ paddingTop: "20px" }}>
               <iframe
                 title="Map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6021385.344638437!2d103.25686172310103!3d16.036623799459683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31157a4d736a1e5f%3A0xb03bb0c9e2fe62be!2zVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1679290776269!5m2!1svi!2s"
@@ -39,8 +39,8 @@ const Contact = () => {
               ></iframe>
             </div>
           </form>
-          <div class="row">
-            <div class="col-sm">
+          <div class="flex flex-wrap ">
+            <div class="relative sm:flex-grow sm:flex-1">
               <form className="">
                 <h4>Thông tin liên hệ</h4> <br />
                 <div>
@@ -51,7 +51,7 @@ const Contact = () => {
                 </div>
               </form>
             </div>
-            <div class="col-sm">
+            <div class="relative sm:flex-grow sm:flex-1">
               <form className="shadow">
                 <h4>Biểu mẫu liên hệ</h4> <br />
                 <div>
