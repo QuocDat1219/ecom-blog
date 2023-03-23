fadc548a64f10063346b09745f1ecb880aaed521
thuan <lapthuan0805@gmail.com>
2023-03-23 10:27:40 +0700

    tailwin css

--- src/components/product/FilterCard.jsx
+++ src/components/product/FilterCard.jsx
@@ -26,16 +26,16 @@ const BrandCard = () => {
     console.log(selectedBrands);
     return (
         <>
-            <div className="card">
+            <div className="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
                 <article className="filter-group">
-                    <div className="card-header">
+                    <div className="py-3 px-6 mb-0 bg-gray-200 border-b-1 border-gray-300 text-gray-900">
                         <a href="#" data-toggle="collapse" data-target="#collapse_2" aria-expanded="true" className="text-decoration-none">
                             <i className="icon-control fa fa-chevron-down"></i>
                             <h6 className="title">Danh mục sản phẩm </h6>
                         </a>
                     </div>
-                    <div className="filter-content collapse show" id="collapse_2">
-                        <div className="card-body">
+                    <div className="filter-content hidden opacity-100 block" id="collapse_2">
+                        <div className="flex-auto p-6">
                             {categorys.map((item, index) => (
                                 <label className="custom-control custom-checkbox" key={item._id}>
                                     <input
@@ -46,7 +46,7 @@ const BrandCard = () => {
                                         onChange={handleCheckboxCategoryChange}
                                     />
                                     <div className="custom-control-label">{item.title}</div>
-                                    {/* <b className="badge badge-pill badge-light float-right">120</b>  </div> */}
+                                    {/* <b className="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded rounded-full py-1 px-3 bg-gray-100 text-gray-800 hover:bg-gray-200 float-right">120</b>  </div> */}
                                 </label>
                             ))}
 
@@ -55,14 +55,14 @@ const BrandCard = () => {
 
                 </article>
                 <article className="filter-group">
-                    <div className="card-header">
+                    <div className="py-3 px-6 mb-0 bg-gray-200 border-b-1 border-gray-300 text-gray-900">
                         <a href="#" data-toggle="collapse" data-target="#collapse_5" aria-expanded="false" className="text-decoration-none">
                             <i className="icon-control fa fa-chevron-down"></i>
                             <h6 className="title">Nhãn hàng </h6>
                         </a>
                     </div>
-                    <div className="filter-content collapse in" id="collapse_5">
-                        <div className="card-body">
+                    <div className="filter-content hidden in" id="collapse_5">
+                        <div className="flex-auto p-6">
                             {brands.map((item, index) => (
                                 <label className="custom-control custom-radio">
                                     <input
@@ -82,7 +82,7 @@ const BrandCard = () => {
                     </div>
                 </article>
                 <div class="d-grid gap-2 my-2 mx-auto">
-                    <button className="btn btn-primary px-5"><i class="fa fa-filter" aria-hidden="true"></i>Lọc</button>
+                    <button className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600 px-5"><i class="fa fa-filter" aria-hidden="true"></i>Lọc</button>
 
                 </div>
             </div>
--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -6,10 +6,10 @@ const Product = () => {
 
     return (
         <>
-            <div className="row">
+            <div className="flex flex-wrap ">
                 {products.map((item, index) => (
-                    <div className="col-md-4" key={item._id}>
-                        <figure className="card card-product-grid">
+                    <div className="md:w-1/3 pr-4 pl-4" key={item._id}>
+                        <figure className="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 card-product-grid">
                             <div className="img-wrap">
                                 <img src={item.imagesThumbnail} width={30} height={30} />
 
@@ -23,7 +23,7 @@ const Product = () => {
                                 </div>
                             </figcaption>
                             <div >
-                                <a href="#" className="btn btn-outline-primary btn-sm btn-block text-decoration-none"><i className="fa fa-search-plus"></i> Xem thêm</a>
+                                <a href="#" className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded  no-underline text-blue-600 border-blue-600 hover:bg-blue-600 hover:text-white bg-white hover:bg-blue-600 py-1 px-2 leading-tight text-xs  block w-full text-decoration-none"><i className="fa fa-search-plus"></i> Xem thêm</a>
                             </div>
                         </figure>
                     </div>
