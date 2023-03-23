3bcd233cbba18e76eceb8d991bf39ba4ce8ce93f
thuan <lapthuan0805@gmail.com>
2023-03-23 18:04:12 +0700

    new

--- src/components/product/allproduct.jsx
+++ src/components/product/allproduct.jsx
@@ -9,23 +9,26 @@ import BrandSillder from "./brandSlider";
 const AllProduct = () => {
     return (
         <>
-            <div className="p-5">
-                <BrandSillder />
-            </div>
-            <Path />
-            <section >
-                <div className="container max-w-screen-xl mx-auto px-4">
-                    <div className="flex flex-col md:flex-row -mx-4">
-                        <FilterCard />
-
-                        <main className="md:w-2/3 lg:w-3/4 w-4/4 px-3">
-
-                            <Product />
-
-                        </main>
-                    </div>
+            <div className="containers">
+                <div className="p-5">
+                    <BrandSillder />
                 </div>
-            </section>
+                <Path />
+                <section >
+                    <div className="container max-w-screen-xl mx-auto px-4">
+                        <div className="flex flex-col md:flex-row -mx-4">
+                            <FilterCard />
+
+                            <main className="md:w-2/3 lg:w-3/4 w-4/4 px-3">
+
+                                <Product />
+
+                            </main>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
 
 
         </>
