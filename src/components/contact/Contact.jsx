014e6e2e00ad8564d1d8e0ffbd274914bc5c11bc
BaoxRosie1 <94978764+BaoxRosie1@users.noreply.github.com>
2023-03-24 10:55:21 +0700

    baooocommit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,28 +7,35 @@ const Contact = () => {
   return (
     <>
       <section className="contact mb">
-        <div className="containers">
+        <div className="containers ">
           <form className="shadow">
             <h4>Tìm kiếm cửa hàng</h4> <br />
-            <div class="row">
-              <div class="col-sm">
-                <select class="browser-default custom-select">
+            <div className="flex flex-wrap ">
+              <div className="relative sm:flex-grow sm:flex-1">
+                <select
+                  id="countries"
+                  class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[260px] sm:w-full"
+                >
                   <option selected>Chọn tỉnh, thành phố</option>
-                  <option value="1">Cần Thơ</option>
-                  <option value="2">Thành phố Hồ Chí Minh</option>
-                  <option value="3">Bình Dương</option>
+                  <option value="US">United States</option>
+                  <option value="CA">Canada</option>
+                  <option value="FR">France</option>
+                  <option value="DE">Germany</option>
                 </select>
               </div>
-              <div class="col-sm">
-                <select class="browser-default custom-select">
+              <div className="relative sm:flex-row sm:flex-1">
+                <select
+                  id="countries"
+                  class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block sm:w-full p-2.5 w-[260px]"
+                >
                   <option selected>Chọn quận, huyện</option>
                 </select>
               </div>
-              <div class="col-sm">
-                <button class="btntk">Tìm kiếm</button>
+              <div className="relative sm:flex-grow sm:flex-1 sm:w-full w-[100%] ">
+                <button className="btntk">Tìm kiếm</button>
               </div>
             </div>
-            <div class="row" style={{ paddingTop: "20px" }}>
+            <div className="flex flex-wrap " style={{ paddingTop: "20px" }}>
               <iframe
                 title="Map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6021385.344638437!2d103.25686172310103!3d16.036623799459683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31157a4d736a1e5f%3A0xb03bb0c9e2fe62be!2zVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1679290776269!5m2!1svi!2s"
@@ -39,8 +46,8 @@ const Contact = () => {
               ></iframe>
             </div>
           </form>
-          <div class="row">
-            <div class="col-sm">
+          <div class="flex flex-wrap ">
+            <div class="relative sm:flex-grow sm:flex-1">
               <form className="">
                 <h4>Thông tin liên hệ</h4> <br />
                 <div>
@@ -51,7 +58,7 @@ const Contact = () => {
                 </div>
               </form>
             </div>
-            <div class="col-sm">
+            <div className="relative sm:flex-grow sm:flex-1">
               <form className="shadow">
                 <h4>Biểu mẫu liên hệ</h4> <br />
                 <div>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import img from "../images/sacdien.jpg";
 import Back from "../common/Back";
 import "../home/featured/Featured.css";
-import FeaturedCard from "../home/featured/FeaturedCard";
 import img2 from "../images/linkkien.jpg";
 import img3 from "../images/tramsac.jpg";
 import "./Services.css";
@@ -11,14 +10,21 @@ const Services = () => {
     <>
       <section className="services mb">
         <Back name="Dịch vụ" title="Các dịch vụ của chúng tôi" cover={img} />
-        <div class="container">
-          <div class="row">
-            <div class="col-sm">
-              <img class="anh" src={img2} alt="linh kien" />
+        <div className="container mx-auto sm:px-4 justify-items-center">
+          <div className="flex flex-wrap justify-items-center">
+            <div className="relative sm:flex-grow sm:flex-1 sm:w-[480px]">
+              <img
+                className="lg:h-[650px] lg:w-[800px] sm:h-[500px] rounded-[20px]"
+                src={img2}
+                alt="linh kien"
+              />
             </div>
-            <div class="col-sm" id="text1">
-              <p>
-                <h2 class="tieude">
+            <div
+              className="relative sm:flex-grow sm:flex-1 w-[500px] sm:h-[250px] h-[500px]  justify-items-center sm:ml-[30px]"
+              id="text1"
+            >
+              <p className="pl-[20px] sm:pt-[30px]">
+                <h2 className="tieude">
                   Một hệ thống sạc cần đảm bảo sạc nhanh, sạc an toàn và luôn
                   sẵn sàng
                 </h2>
@@ -33,24 +39,51 @@ const Services = () => {
               </p>
             </div>
           </div>
-        </div>{" "}
-        <div class="imgdai">
+        </div>
+
+        <div className="imgdai">
           <Back cover={img3} />
         </div>
-        <div style={{textAlign:"center", paddingTop:"20px"}}>
-          <h3 style={{paddingBottom:"20px"}}>Dịch vụ vận hành và bảo hành</h3>
-          <div class="row">
-            <div class="col-sm">
-              <img src={img2} alt="" class="imgblog" />
-              <p>Tên gì đó</p>
+        <div className="text-center pt-[20px]">
+          <h3 className="text-[24px] pb-[20px]">
+            Dịch vụ vận hành và bảo hành
+          </h3>
+          <div class="flex flex-wrap justify-between">
+            <div class="w-full md:w-1/3 px-4 mb-8 md:mb-0">
+              <div class="relative h-0 pb-[100%] mb-4">
+                <img
+                  src="https://picsum.photos/id/1018/400"
+                  alt="Item 1"
+                  class="absolute inset-0 w-full h-full object-cover rounded-[20px]"
+                />
+              </div>
+              <p class="text-gray-700 leading-relaxed text-center mt-4">
+                Bài viết 1
+              </p>
             </div>
-            <div class="col-sm">
-              <img src={img2} alt="" class="imgblog" />
-              <p>Tên gì đó</p>
+            <div class="w-full md:w-1/3 px-4 mb-8 md:mb-0">
+              <div class="relative h-0 pb-[100%] mb-4">
+                <img
+                  src="https://picsum.photos/id/1020/400"
+                  alt="Item 2"
+                  class="absolute inset-0 w-full h-full object-cover rounded-[20px]"
+                />
+              </div>
+              <p class="text-gray-700 leading-relaxed text-center mt-4">
+                Bài viết 2
+              </p>
             </div>
-            <div class="col-sm">
-              <img src={img2} alt="" class="imgblog" />
-              <p>Tên gì đó</p>
+            <div class="w-full md:w-1/3 px-4 mb-8 md:mb-0">
+              <div class="relative h-0 pb-[100%] mb-4">
+                <img
+                  src="https://picsum.photos/id/1033/400"
+                  alt="Item 3"
+                  class="absolute inset-0 w-full h-full object-cover rounded-[20px]"
+                />
+              </div>
+              <p class="text-gray-700 leading-relaxed text-center mt-4">
+                Bài viết 3
+              </p>
             </div>
           </div>
         </div>
