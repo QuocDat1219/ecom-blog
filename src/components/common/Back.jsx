009c33eb3f7d66aaee7e31213fe08ce96b261eb8
thuan <lapthuan0805@gmail.com>
2023-03-26 12:19:18 +0700

    black new

--- src/components/common/Back.jsx
+++ src/components/common/Back.jsx
@@ -6,7 +6,7 @@ const Back = ({ name, title, cover }) => {
       <div className='back'>
         <div className='containers pt-[40px]'>
           <span className="text-[24px] pt-[40px] text-black font-bold">{name}</span>
-          <h1 className="text-[50px] text-color-title font-bold">{title}</h1>
+          <h1 className="text-[50px] text-black font-bold">{title}</h1>
         </div>
         <img src={cover} alt='' />
       </div>
