abf469dec081915ec61a8a96878d4662336d2f77
LAPTOP-3NFIFOLQ\Tuan Anh <nguyenhoangtuananh.nt16@gmail.com>
2023-03-28 11:42:27 +0700

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,13 +10,17 @@ import Sliderimg from "./Slide";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 const About = () => {
-  const [showText, setShowText] = useState(false); // khai báo state cho showText và set giá trị mặc định là false
+  const [showText, setShowText] = useState(false);
   const [showTexFirst, setShowTextFirst] = useState(false);
+  const [showTexSecond, setShowTextSecond] = useState(false);
   const handleClick = () => {
     setShowText(!showText);
   };
   const handleClickFirst = () => {
     showTexFirst(!setShowTextFirst);
+  };
+  const handleClickLast = () => {
+    showTexSecond(!setShowTextSecond);
   };
   return (
     <>
@@ -29,13 +33,13 @@ const About = () => {
           <div class="image-container py-2">
             <img src={imgAb} />
           </div>
-          <div className="text-container text-text-color bg-color-card break-words overflow-y-scroll shadow">
-            <p>
+          <div className="text-container text-text-color bg-color-card break-words shadow">
+            <p className="mt-5 ml-5 mr-5 ">
               EverEV là một trong những đơn vị tiên phong trong lĩnh vực sạc ô
               tô điện. Công ty là nhà phân phối chính hãng của nhiều thương hiệu
               sạc ô tô hàng đầu trên thế giới như Starcharge.
             </p>
-            <p>
+            <p className="ml-5 mr-5 mb-5">
               Được sự ủy quyền của Starchage, EverEV chính thức trở thành đơn vị
               thực hiện công tác bảo trì và sửa chữa các sạc ô Starcharge trên
               toàn bộ lãnh thổ Việt Nam.
@@ -47,7 +51,10 @@ const About = () => {
         </div>
 
         <div className="containers flexs mtop">
-          <div className="text-container text-text-color bg-color-card break-words overflow-y-scroll shadow">
+          <div
+            className="text-container text-text-color bg-color-card break-words overflow-y-scroll shadow"
+            style={{ height: "480px" }}
+          >
             <div className="about_diveder"></div>
 
             <div className="btn_show">
@@ -158,6 +165,67 @@ const About = () => {
               </p>
             </div>
             {showTexFirst && (
+              <div className="div_show">
+                <span className="text_show">
+                  Công ty đã trúng thầu dự án lắp đặt 2 sạc ô tô xe điện tại
+                  siêu thị Aeon Hà Đông. Có thể xem đây là hai trong số những
+                  sạc ô tô xe điện đầu tiên tại Việt Nam. Với tiền đề này, công
+                  ty tiếp tục nghiên cứu sâu và mở rộng tiếp xúc với các đối tác
+                  về sạc ô tô lớn trên thế giới.
+                </span>
+              </div>
+            )}
+
+            <div className="about_diveder"></div>
+
+            <div className="btn_showFirst">
+              <div
+                className="toggle-button-container"
+                style={{ display: "inline-flex" }}
+              >
+                <p
+                  className="toggle-button"
+                  onClick={() => setShowTextSecond(!showTexSecond)}
+                  style={{ flexDirection: "row-reverse" }}
+                >
+                  {showTexSecond ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-chevron-up"
+                    >
+                      <path d="M18 15l-6-6-6 6" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-chevron-down"
+                    >
+                      <path d="M6 9l6 6 6-6" />
+                    </svg>
+                  )}
+                </p>
+              </div>
+              <p>
+                <span className="arrow">
+                  Cùng thời gian đó, ban lãnh đạo công ty cũng đã nhận thức rõ
+                  về xu thế phát triển tất yếu của thị trường ô tô điện cùng với
+                  các dịch vụ đi kèm.
+                </span>
+              </p>
+            </div>
+            {showTexSecond && (
               <div className="div_show">
                 <span className="text_show">
                   Công ty đã trúng thầu dự án lắp đặt 2 sạc ô tô xe điện tại
