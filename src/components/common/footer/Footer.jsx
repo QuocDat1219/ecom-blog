04bf4a84aca095cefe2e4f68f2d52c0dfc6aa322
LAPTOP-3NFIFOLQ\Tuan Anh <nguyenhoangtuananh.nt16@gmail.com>
2023-03-29 11:21:11 +0700

    taunhanh-59

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,45 +1,39 @@
 import React from "react";
+import "./Footer.css";
+import logo from "../../images/logo.jpg";
 const Footer = () => {
   return (
     <>
       <div className="bg-color-card w-100% h-0.5"></div>
       <footer>
-        <div className="bg-color-basic p-3">
-          <div className="footer-content flex flex-col md:flex-row md:justify-center md:items-center text-center md:text-left">
-            <div className="company-info md:mr-8">
-              <div className="w-[120px] h-[80px] ml-auto mr-auto sm:w-[230px] sm:h-[150px]">
-                <img
-                  src="https://everev.vn/wp-content/uploads/2023/03/everev-logo-1-e1678267237257.png"
-                  alt=""
-                />
+        <div className="bg-color-card p-3">
+          <div className="footer-content grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="company-info md:mr-8 text-center">
+              <div className="w-[120px] h-[120px] ml-auto mr-auto sm:w-[230px] sm:h-[150px]">
+                <img src={logo} alt="" />
               </div>
-              <h3 className=" text-color-title text-center text-[20px] font-bold">
-                Công ty Cổ phần EverEV
+              <h3 className="text-color-title text-[50px] font-bold mt-2 ">
+                THÀNH ĐẠT
               </h3>
-              <p className="text-text-color">
-                Ever Ev là công ty phân phối, lắp đặt và vận hành sạc ô tô điện
-                hàng đầu Việt Nam
-              </p>
             </div>
-
-            <div className="md:mr-8">
-              <h3 className="text-color-title text-center text-[20px] font-bold">
+            <div className="contact-info ">
+              <h3 className="text-color-title text-2xl font-bold text-center">
                 Liên hệ
               </h3>
               <div className="text-text-color">
                 <p>
-                  300B Minh Khai, Phường Minh Khai, Quận Hai Bà Trưng, Hà Nội,
-                  Việt Nam
+                  <i className="fas fa-map-marker-alt mr-2"></i>
+                  Địa chỉ liên hệ: 92 Tổ 3, Ấp 1, Xã Sông Trầu, Huyện Trảng Bom,
+                  Tỉnh Đồng Nai.
                 </p>
-                <p>0909184567</p>
-                <p>info@everev.vn</p>
+                <a href="tel:#" className="text-text-color ">
+                  <i className="fas fa-phone-alt fa-rotate-90 fa-xs mr-2"></i>
+                  Điện thoại: 0931116080
+                </a>
               </div>
             </div>
-
-            <div className="justify-center items-center">
-              <h3 className="text-color-title text-center text-[20px] font-bold">
-                Địa chỉ
-              </h3>
+            <div className="address-info text-center">
+              <h3 className="text-color-title text-2xl font-bold">Địa chỉ</h3>
               <iframe
                 title="EverEV Map"
                 className="w-[200] h-[150] mr-auto ml-auto"
@@ -51,10 +45,10 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="bg-color-card w-100% h-0.5"></div>
-        <div className="p-3 bg-color-basic">
-          <p className="text-center text-text-title font-bold">
-            &copy; 2023 EverEV. All rights reserved.
+        <div className="bg-color-title w-100% h-0.5"></div>
+        <div className="p-3 bg-color-title">
+          <p className="text-center text-text-cl font-bold">
+            &copy; 2023 THÀNH ĐẠT. All rights reserved.
           </p>
         </div>
       </footer>
