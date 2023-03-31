98a7850053bd0dec9e92afb83834a227467e29da
BaoxRosie1 <94978764+BaoxRosie1@users.noreply.github.com>
2023-03-31 13:03:55 +0700

    baosanpham

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,19 +1,29 @@
 import { products } from "../data/Data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 const Product = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("https://ecom-oto.vercel.app/api/products/");
+      setData(result.data.products);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <div className="mt-4 grid gap-y-10 gap-x-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-        {products.map((item) => (
+        {data.map((item) => (
           <div
             key={item._id}
             className="group relative bg-color-card rounded-md shadow overflow-hidden"
           >
             <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-color-basic group-hover:opacity-75 lg:aspect-none lg:h-50">
               <img
-                src={item.imagesThumbnail}
+                src={item.imagesDefault}
                 className="h-full w-full object-cover object-center group-hover:opacity-75"
               />
             </div>
@@ -21,12 +31,12 @@ const Product = () => {
             <div className="mt-2 flex justify-center pl-[10px] py-1 overflow-hidden">
               <div>
                 <p className="mt-1 text-sm text-text-color  text-center">
-                  {item.category}
+                  {item.idCategory}
                 </p>
                 <h3 className="text-sm text-text-color text-center">
                   <a href={`/productdetail/${item._id}`}>
                     <span aria-hidden="true" className="absolute inset-0" />
-                    {item.title.slice(0, 20)}
+                    {item.name}
                   </a>
                 </h3>
               </div>
--- src/components/product/productDetail.jsx
+++ src/components/product/productDetail.jsx
@@ -1,92 +1,160 @@
-import { products } from "../data/Data";
+// import { products } from "../data/Data";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { useParams } from "react-router-dom";
 import Path from "./path";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
+import axios from "axios";
+import Comment from "../contentsBlog/Comment";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+import { Square3Stack3DIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 
 const ProductDetail = () => {
-  const { id } = useParams();
+  const id = useParams();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [data, setData] = useState([]);
+  const [product, setProduct] = useState(null);
 
   const onSlide = (currentIndex) => {
     setCurrentIndex(currentIndex);
   };
-  const record = products.find((record) => record._id === id);
 
+  useEffect(() => {
+    axios("https://ecom-oto.vercel.app/api/products/").then((response) => {
+      const pro = response.data.products;
+      setData(pro);
+    });
+
+    // data2 = data.find((record) => record._id === id);
+    // console.log(data2);
+  }, []);
+  useEffect(() => {
+    const foundProduct = data.find((p) => p._id == Number(id.id));
+
+    setProduct(foundProduct);
+    console.log(product);
+  });
+  const datachitiet = [
+    {
+      label: "Chi tiết sản phẩm",
+      value: "dashboard",
+      icon: Square3Stack3DIcon,
+      desc: `Mô tả đi Lập Thuận`,
+    },
+    {
+      label: "Đánh giá",
+      value: "profile",
+      icon: UserCircleIcon,
+      desc: `Đánh giá đi Lập Thuận`,
+    },
+  ];
   return (
-    <>
-      <div className="containers">
-        <Path data={record.title} />
-      </div>
-      <section class="overflow-hidden bg-white py-7 ">
-        <div class="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
-          <div class="flex flex-wrap -mx-4">
-            <div class="w-full mb-8 md:w-1/2 md:mb-0">
-              <div class="sticky top-0 z-30 overflow-hidden ">
-                <ImageGallery
-                  items={record.imagesDetail}
-                  showFullscreenButton={true}
-                  useBrowserFullscreen={true}
-                  onSlide={onSlide}
-                  thumbnailPosition={"left"}
-                />
-
-                <div class="px-6 pb-6 mt-6 border-t border-gray-300  ">
-                  <div class="flex flex-wrap items-center mt-6">
-                    <h2 class="text-lg font-bold text-gray-700 ">Mô tả</h2>
-                  </div>
-                  <div class="mt-2 px-7">{record.detail}</div>
-                </div>
-              </div>
+    <div>
+      <>
+        {product == null ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            <div className="containers">
+              <Path data={product.name} />
             </div>
-            <div class="w-full px-4 md:w-1/2 ">
-              <div class="lg:pl-20">
-                <div class="mb-8 ">
-                  <h2 class="max-w-xl mb-6 text-2xl font-bold  md:text-4xl text-color-title">
-                    {record.title}
-                  </h2>
-                  <p class="inline-block mb-6 text-xl font-bold text-text-color  ">
-                    <span>Danh mục: {record.category}</span>
-                  </p>
-                  <p class="max-w-md text-text-color">{record.description}</p>
-                </div>
-
-                <div class="mb-8 ">
-                  <div>
-                    <div class="flex flex-wrap -mb-2">
-                      <a href="">
-                        <button class="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400 hover:text-black bg-color-button text-text-color">
-                          Zalo
-                        </button>
-                      </a>
+            <section class="overflow-hidden bg-white py-7 ">
+              <div class="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
+                <div class="flex flex-wrap -mx-4">
+                  <div class="w-full mb-8 md:w-1/2 md:mb-0">
+                    <div class="sticky top-0 z-30 overflow-hidden ">
+                      <ImageGallery
+                        items={product.imagesDetail}
+                        showFullscreenButton={true}
+                        useBrowserFullscreen={true}
+                        onSlide={onSlide}
+                        thumbnailPosition={"left"}
+                      />
                     </div>
                   </div>
-                </div>
-                <div class="mb-8">
-                  <h2 class=" pb-1 mb-4 text-2xl font-bold border-b border-blue-300 ">
-                    Liên hệ
-                  </h2>
-                  <div class="flex flex-wrap -mx-2 -mb-2">
-                    <button class="p-1 mb-2 mr-3 bg-color-button">
-                      <FaFacebookF className="w-6 h-6 text-text-color" />
-                    </button>
-                    <button class="p-1 mb-2 mr-3 bg-color-button">
-                      <FaTwitter className="w-6 h-6 text-text-color" />
-                    </button>
-                    <button class="p-1 mb-2 mr-3 bg-color-button">
-                      <HiOutlineMail className="w-6 h-6 text-text-color" />
-                    </button>
+                  <div class="w-full px-4 md:w-1/2 ">
+                    <div class="lg:pl-20">
+                      <div class="mb-8 ">
+                        <h2 class="max-w-xl mb-6 text-2xl font-bold  md:text-4xl text-color-title">
+                          {product.name}
+                        </h2>
+                        <p class="inline-block mb-6 text-xl font-bold text-text-color  ">
+                          <span>Danh mục: {product.idCategory}</span>
+                        </p>
+                        <p class="max-w-md text-text-color">
+                          {product.description}
+                        </p>
+                      </div>
+
+                      <div class="mb-8 ">
+                        <div>
+                          <div class="flex flex-wrap -mb-2">
+                            <a href="">
+                              <button class="px-4 py-2 mb-2 mr-4 font-semibold border rounded-md hover:border-blue-400 hover:text-black bg-color-button text-text-color">
+                                Zalo
+                              </button>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="mb-8">
+                        <h2 class=" pb-1 mb-4 text-2xl font-bold border-b border-blue-300 ">
+                          Liên hệ
+                        </h2>
+                        <div class="flex flex-wrap -mx-2 -mb-2">
+                          <button class="p-1 mb-2 mr-3 bg-color-button">
+                            <FaFacebookF className="w-6 h-6 text-text-color" />
+                          </button>
+                          <button class="p-1 mb-2 mr-3 bg-color-button">
+                            <FaTwitter className="w-6 h-6 text-text-color" />
+                          </button>
+                          <button class="p-1 mb-2 mr-3 bg-color-button">
+                            <HiOutlineMail className="w-6 h-6 text-text-color" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    
                   </div>
+                  <div class="px-6 pb-6 mt-6 border-t w-[100%] border-gray-300  ">
+                      <Tabs value="dashboard">
+                        <TabsHeader>
+                          <Tab value={"mota"}>
+                            <div className="flex items-center gap-2">Mô tả</div>
+                          </Tab>
+                          <Tab value={"danhgia"}>
+                            <div className="flex items-center gap-2">
+                              Đánh giá
+                            </div>
+                          </Tab>
+                        </TabsHeader>
+                        <TabsBody>
+                          <TabPanel value={"mota"}>
+                            {product.description}
+                          </TabPanel>
+                        </TabsBody>
+                        <TabsBody>
+                          <TabPanel  value={"danhgia"} className="">
+                           <Comment/>
+                          </TabPanel>
+                        </TabsBody>
+                      </Tabs>
+                    </div>
                 </div>
               </div>
-            </div>
+            </section>
           </div>
-        </div>
-      </section>
-    </>
+        )}
+      </>
+    </div>
   );
 };
 export default ProductDetail;
