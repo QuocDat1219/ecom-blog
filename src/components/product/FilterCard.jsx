04d8ebc36f96f98df4d63f8ea7a7c34bc8fd66e8
LAPTOP-3NFIFOLQ\Tuan Anh <nguyenhoangtuananh.nt16@gmail.com>
2023-03-31 13:41:57 +0700

    update_Tuananh

--- src/components/product/FilterCard.jsx
+++ src/components/product/FilterCard.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter, faGlasses } from "@fortawesome/free-solid-svg-icons";
 import { categorys, brands } from "../data/Data";
+import axios from "axios";
 const Filters = () => {
-  let queryParams;
+  const [categories, setCategories] = useState([]);
+  const [brands, setBrands] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://ecom-oto.vercel.app/api/category/"
+        );
+        const data = response.data.category;
+        setCategories(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://ecom-oto.vercel.app/api/brand/"
+        );
+        const data = response.data;
+        setBrands(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <aside className="md:w-1/3 lg:w-1/4 px-4 py-4">
       <a className="md:hidden mb-5  w-full text-center px-4 py-2 inline-block text-lg text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:text-blue-600">
@@ -15,20 +46,20 @@ const Filters = () => {
         <h3 className="font-semibold mb-2 text-color-title">Danh mục</h3>
 
         <ul className="space-y-1">
-          {categorys.map((item) => (
-            <li key={item._id}>
+          {categories.map((category) => (
+            <li key={category._id}>
               <div class="flex items-center">
                 <input
                   id="default-checkbox"
                   type="checkbox"
-                  value={item._id}
+                  value={category._id}
                   class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2 "
                 />
                 <label
                   for="checked-checkbox"
                   class="ml-2 text-sm font-medium text-gray-900 "
                 >
-                  {item.title}
+                  {category.name}
                 </label>
               </div>
             </li>
@@ -36,23 +67,22 @@ const Filters = () => {
         </ul>
 
         <hr className="my-4 text-color-basic" />
-
         <h3 className="font-semibold mb-2 text-color-title">Nhãn hàng</h3>
         <ul className="space-y-1">
-          {brands.map((item) => (
-            <li key={item._id}>
+          {brands.map((data) => (
+            <li key={data._id}>
               <div class="flex items-center">
                 <input
                   id="default-checkbox"
                   type="checkbox"
-                  value={item._id}
+                  value={data._id}
                   class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2  "
                 />
                 <label
                   for="checked-checkbox"
                   class="ml-2 text-sm font-medium text-gray-900 "
                 >
-                  {item.title}
+                  {data.title}
                 </label>
               </div>
             </li>
