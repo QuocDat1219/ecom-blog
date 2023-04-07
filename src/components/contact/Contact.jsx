09cf1fdd77aa9864efeb2aea41d19eda0bd949cc
QuocDat1219 <lenguyenquocdat1219@gmail.com>
2023-04-07 20:52:58 +0700

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,53 @@
 import React from "react";
-import img from "../images/pricing.jpg";
-import Back from "../common/Back";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from 'react-toastify';
 import "./contact.css";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [description, setDescription] = useState("");
+  var checkMail =
+    /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  const handelSubmitContact = async (e) => {
+    e.preventDefault();
+    if (name == "" || email == "" || description == "" || phone == "" || address == "") {
+      toast.warning("Vui lòng không để trống thông tin");
+      return;
+    } else if (!checkMail.test(email) || email.length == "") {
+      toast.warning("Email không hợp lệ");
+      return;
+    }
+    await axios.post("http://localhost:5000/api/contact/", {
+      name: name,
+      email, email,
+      phone: phone,
+      address: address,
+      description: description
+    }).then((data) => {
+      if (data.data.status) {
+        toast.success("Gửi biểu mẫu liên hệ thàng công");
+        setName("");
+        setEmail("");
+        setPhone("");
+        setAddress("");
+        setDescription("");
+      } else
+        toast.danger("Lỗi")
+    })
+  }
   return (
     <>
       <section className="contact mb">
         <div className="containers ">
           <div className="shadow mt-4 mb-4 rounded-md bg-color-card overflow-auto">
-            <form className=" justify-center items-center ">
-              <h4 className="lg:text-left text-[40px] font-bold text-color-title">
-                Vị trí
-              </h4>{" "}
-            </form>
+            <h4 className="lg:text-left text-[40px] font-bold text-color-title">
+              Vị trí
+            </h4>{" "}
             <div className="flex flex-wrap p-4 ">
               <iframe
                 className="rounded-lg"
@@ -29,21 +63,19 @@ const Contact = () => {
 
           <div className="flex flex-wrap ">
             <div className="relative sm:flex-grow sm:flex-1">
-              <form className="text-[1.4rem]">
-                <h4>Thông tin liên hệ</h4> <br />
-                <div>
-                  <p>
-                    THÀNH ĐẠT
-                    <br /> Địa chỉ : 92 Tổ 3, Ấp 1, Xã Sông Trầu, Huyện Trảng
-                    Bom, Tỉnh Đồng Nai. <br />
-                    Điện Thoại: 0931116080
-                  </p>
-                </div>
-              </form>
+              <h4>Thông tin liên hệ</h4> <br />
+              <div>
+                <p>
+                  THÀNH ĐẠT
+                  <br /> Địa chỉ : 92 Tổ 3, Ấp 1, Xã Sông Trầu, Huyện Trảng
+                  Bom, Tỉnh Đồng Nai. <br />
+                  Điện Thoại: 0931116080
+                </p>
+              </div>
             </div>
             <div className="relative sm:flex-grow sm:flex-1">
               <div className="shadow rounded-md bg-color-card">
-                <form>
+                <form onSubmit={handelSubmitContact}>
                   <h4 className="font-blod text-color-title text-[24px]">
                     Biểu mẫu liên hệ
                   </h4>{" "}
@@ -53,24 +85,30 @@ const Contact = () => {
                       type="text"
                       className="flex-grow border-gray-400 border-2 p-2 rounded-md mr-4 "
                       placeholder="Họ và tên"
+                      onChange={(e) => setName(e.target.value)}
                     />
                     <input
                       type="email"
                       className="flex-grow border-gray-400 border-2 p-2 rounded-md mr-4 "
                       placeholder="Địa chỉ email"
+                      onChange={(e) => setEmail(e.target.value)}
                     />
                     <input
                       type="number"
                       className="flex-grow border-gray-400 border-2 p-2 rounded-md mr-4 "
                       placeholder="Số điện thoại"
+                      onChange={(e) => setPhone(e.target.value)}
                     />
                     <input
                       type="text"
                       className="flex-grow border-gray-400 border-2 p-2 rounded-md mr-4 "
                       placeholder="Địa chỉ"
+                      onChange={(e) => setAddress(e.target.value)}
                     />
                   </div>
-                  <textarea cols="28" rows="10"></textarea>
+                  <textarea cols="28" rows="10"
+                    onChange={(e) => setDescription(e.target.value)
+                    }></textarea>
                   <button className="bg-color-button w-[100%] rounded-md text-white">
                     Gửi liên hệ
                   </button>
