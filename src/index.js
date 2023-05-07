04cf599fc2b8a2afc1142aa574fb6360eb900a01
QuocDat1219 <lenguyenquocdat1219@gmail.com>
2023-05-07 14:14:15 +0700

    fixbug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-const root = ReactDOM.createRoot(document.getElementById("root"))
+import { ThemeProvider } from "@material-tailwind/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </React.StrictMode>
   </Provider>
-)
+);
