7c311f2e96a8913b14677d0c02f83307090d76c2
QuocDat1219 <lenguyenquocdat1219@gmail.com>
2023-05-15 21:46:56 +0700

    new

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API = axios.create({ baseURL: "https://ecom-z3we.onrender.com" });
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ecom-oto.vercel.app",
 });
 
 API.interceptors.request.use((req) => {
