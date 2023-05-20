1845f2fe36889c6ae3e64fc725008fb6bc45fc39
BaoxRosie1 <94978764+BaoxRosie1@users.noreply.github.com>
2023-05-20 21:07:22 +0700

    dolachuyencuamay

--- src/components/Headergiohang/headergiohang.jsx
+++ src/components/Headergiohang/headergiohang.jsx
@@ -1,4 +1,4 @@
-import { RiShoppingCartFill } from "react-icons/ri";
+import { RiShoppingCartFill, RiUserSharedFill } from "react-icons/ri";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,25 +15,27 @@ const Headergiohang = () => {
     dispatch(logout());
   };
   return (
-    <nav className="flex p-4  text-black">
-      <div className="lg:ml-[93%] ml-[75%]">
+    <nav className="flex text-black">
+      <div className="lg:ml-[93%] ml-[70%]">
+        {cartItems.length > 0 && (
+          <span className="cart-basket z-10 absolute ml-[34px] mt-[3px] bg-red-500 rounded-full flex items-center justify-center w-[24px] h-[24px] text-white">
+            {cartItems.length}
+          </span>
+        )}
         <Link to="/cart">
-          <RiShoppingCartFill className="w-10 h-10 text-[#6698ff]" />
+          <RiShoppingCartFill className="w-10 h-10 mt-[10px] text-[#6698ff]"/>
         </Link>
       </div>
       {isauth == true ? (
         <div className="lg:ml-auto ml-auto">
-          {cartItems.length > 0 && (
-            <span className="cart-basket">{cartItems.length}</span>
-          )}
           <button onClick={() => setOpenDiaLog(true)}>
-            <HiOutlineUserCircle className="w-10 h-10 text-[#6698ff]" />
+            <HiOutlineUserCircle className="w-10 h-10 text-[#6698ff]"/>
           </button>
         </div>
       ) : (
         <div className="lg:ml-auto ml-auto">
           <Link to="/login">
-            <sp className="text-xl">Đăng nhập / Đăng ký</sp>
+            <RiUserSharedFill className="w-10 h-10 mt-[10px] mr-[10px] mb-[10px] text-[#6698ff]"/>
           </Link>
         </div>
       )}
