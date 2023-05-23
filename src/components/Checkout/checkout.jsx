b5551ca413e38d27fc6f27bc6a10bff941bdc207
BaoxRosie1 <94978764+BaoxRosie1@users.noreply.github.com>
2023-05-23 08:36:20 +0700

    kskdkaksd

--- src/components/Checkout/checkout.jsx
+++ src/components/Checkout/checkout.jsx
@@ -1,8 +1,88 @@
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import axios from "axios";
+import { addItem, removeItem } from "../../redux/action/cartActions";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import emty from "../images/empty-cart.png";
 import "./checkout.css";
 const Checkout = () => {
-  return (
+  const userData = localStorage.getItem("token") || "";
+  const [totalPrices, setTotalPrices] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [shipPrices, setShipPrices] = useState(30000);
+  const [voucher, setVoucher] = useState();
+  const [voucherPrices, setVoucherPrices] = useState(0);
+  const [voucherTitle, setVoucherTitle] = useState("");
+
+  const [userId, userEmail, userPassword] = userData.split(":");
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  let cartData = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+  const totalPrice = (price, qty) => {
+    const total = price * qty;
+    const formattedTotal = total.toLocaleString({
+      style: "currency",
+      currency: "VND",
+    });
+    return formattedTotal;
+  };
+
+  const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    cartData.forEach((item) => {
+      totalPrice += item.price * item.qty;
+    });
+
+    return totalPrice;
+  };
+
+  const calculateTotal = () => {
+    let total = 0;
+    total = totalPrices + shipPrices - voucherPrices;
+
+    return total;
+  };
+  useEffect(() => {
+    const totalPrice = calculateTotalPrice();
+    setTotalPrices(totalPrice);
+    const total = calculateTotal();
+    setTotal(total);
+  }, [cartData]);
+
+  const vouchers = [
+    {
+      ma: "123",
+      dis: 10000,
+    },
+    {
+      ma: "Ma1",
+      dis: 15000,
+    },
+  ];
+
+  const addVoucher = () => {
+    const existingVoucher = vouchers.find(
+      (vc) => vc.ma.toLowerCase() === voucher.toLowerCase()
+    );
+
+    if (existingVoucher) {
+      setVoucherTitle(existingVoucher.ma);
+      setVoucherPrices(existingVoucher.dis);
+      toast("Voucher đã nhập thành công");
+    } else {
+      toast("Voucher không tồn tại");
+    }
+  };
+  const handleRemoveFromCart = (id) => {
+    dispatch(removeItem(id));
+  };
+
+  return cartData.length != 0 ? (
     <div classNameName="maincontainer">
       <div className="container mx-auto sm:px-4">
         <div className="py-5 text-center font-bold text-[25px]">Checkout</div>
@@ -10,45 +90,63 @@ const Checkout = () => {
           <div className="md:w-1/3 pr-4 pl-4 md:order-2 mb-4">
             <h4 className="flex justify-between items-center mb-3">
               <span className="text-gray-700">Giỏ hàng của bạn</span>
-              <span className="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none bg-gray-600 text-white hover:bg-gray-700 rounded-full py-1 px-3">
-                3
-              </span>
             </h4>
-            <ul className="flex flex-col pl-0 border rounded border-gray-300 mb-3">
-              <li className="relative  py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between lh-condensed">
-                <div>
-                  <h6 className="my-0">Product name</h6>
-                  <small className="text-gray-700">Brief description</small>
-                </div>
-                <span className="text-gray-700">$12</span>
-              </li>
-              <li className="relative  py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between lh-condensed">
-                <div>
-                  <h6 className="my-0">Second product</h6>
-                  <small className="text-gray-700">Brief description</small>
-                </div>
-                <span className="text-gray-700">$8</span>
-              </li>
-              <li className="relative  py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between lh-condensed">
-                <div>
-                  <h6 className="my-0">Third item</h6>
-                  <small className="text-gray-700">Brief description</small>
-                </div>
-                <span className="text-gray-700">$5</span>
-              </li>
-              <li className="relative py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between bg-gray-100">
-                <div className="text-green-500">
-                  <h6 className="my-0">Promo code</h6>
-                  <small>EXAMPLECODE</small>
-                </div>
-                <span className="text-green-500">-$5</span>
-              </li>
+            {cartData.map((item, index) => (
+              <ul className="flex flex-col pl-0 border rounded border-gray-300 mb-3">
+                <li className="relative  py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between lh-condensed">
+                  <div>
+                    <h6 className="text-gray-700">{item.title} <span className="text-blue-600 text-[15px]"> x {item.qty}</span></h6>
+                  </div>
+                  <span className="text-gray-700">
+                    {new Intl.NumberFormat({
+                      style: "currency",
+                      currency: "VND",
+                    }).format(item.price)}{" "}
+                    VNĐ
+                  </span>
+                </li>
+                {/* <li className="relative py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between bg-gray-100">
+                  <div className="text-green-500">
+                    <h6 className="my-0">Promo code</h6>
+                    <small>EXAMPLE CODE</small>
+                  </div>
+                  <span className="text-green-500">  {new Intl.NumberFormat({
+                          style: "currency",
+                          currency: "VND",
+                        }).format(voucherPrices)}{" "}
+                        VNĐ</span>
+                </li> */}
+                {/* <li className="relative  py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between">
+                  <span>Total (USD)</span>
+                  <strong>$20</strong>
+                </li> */}
+              </ul>
+            ))}
+            <div className="">
+              <ul>
               <li className="relative  py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between">
-                <span>Total (USD)</span>
-                <strong>$20</strong>
-              </li>
-            </ul>
-            <form className="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 p-2">
+                  <span>Vận chuyển (VND)</span>
+                  <span className="text-green-300">
+                        {new Intl.NumberFormat({
+                          style: "currency",
+                          currency: "VND",
+                        }).format(shipPrices)}{" "}
+                        VNĐ
+                      </span>
+                </li>
+                <li className="relative  py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline flex justify-between">
+                  <span>Tổng cộng (VND)</span>
+                  <span className="text-red-500">
+                        {new Intl.NumberFormat({
+                          style: "currency",
+                          currency: "VND",
+                        }).format(total)}{" "}
+                        VNĐ
+                      </span>
+                </li>
+              </ul>
+            </div>
+            {/* <form className="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 p-2">
               <div className="relative flex items-stretch w-full">
                 <input
                   type="text"
@@ -64,7 +162,7 @@ const Checkout = () => {
                   </button>
                 </div>
               </div>
-            </form>
+            </form> */}
           </div>
           <div className="md:w-2/3 pr-4 pl-4 md:order-1">
             <h4 className="mb-3">Billing address</h4>
@@ -99,21 +197,6 @@ const Checkout = () => {
                   </div>
                 </div>
               </div>
-              {/* <div className="mb-3">
-                <label for="username">Username</label>
-                <div className="relative flex items-stretch w-full">
-                  <input
-                    type="text"
-                    className="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded"
-                    id="username"
-                    placeholder="Username"
-                    required
-                  />
-                  <div className="hidden mt-1 text-sm text-red">
-                    Your username is required.
-                  </div>
-                </div>
-              </div> */}
               <div className="mb-3">
                 <label for="email">
                   Email <span className="text-gray-700"></span>
@@ -152,124 +235,23 @@ const Checkout = () => {
                   placeholder=""
                 />
               </div>
-              {/* <div className="flex flex-wrap ">
-                <div className="md:w-2/5 pr-4 pl-4 mb-3">
-                  <label for="country">Country</label>
-                  <select
-                    className="custom-select block w-full"
-                    id="country"
-                    required
-                  >
-                    <option value="">Choose...</option>
-                    <option>United States</option>
-                  </select>
-                  <div className="hidden mt-1 text-sm text-red">
-                    Please select a valid country.
-                  </div>
-                </div>
-                <div className="md:w-1/3 pr-4 pl-4 mb-3">
-                  <label for="state">State</label>
-                  <select
-                    className="custom-select block w-full"
-                    id="state"
-                    required
-                  >
-                    <option value="">Choose...</option>
-                    <option>California</option>
-                  </select>
-                  <div className="hidden mt-1 text-sm text-red">
-                    Please provide a valid state.
-                  </div>
-                </div>
-                <div className="md:w-1/4 pr-4 pl-4 mb-3">
-                  <label for="zip">Zip</label>
-                  <input
-                    type="text"
-                    className="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded"
-                    id="zip"
-                    placeholder=""
-                    required
-                  />
-                  <div className="hidden mt-1 text-sm text-red">
-                    Zip code required.
-                  </div>
-                </div>
-              </div> */}
-              {/* <hr className="mb-4" /> */}
-              {/* <div className="custom-control custom-checkbox">
-                <input
-                  type="checkbox"
-                  className="custom-control-input"
-                  id="same-address"
-                />
-                <label className="custom-control-label" for="same-address">
-                  Shipping address is the same as my billing address
-                </label>
-              </div>
-              <div className="custom-control custom-checkbox">
-                <input
-                  type="checkbox"
-                  className="custom-control-input"
-                  id="save-info"
-                />
-                <label className="custom-control-label" for="save-info">
-                  Save this information for next time
-                </label>
-              </div> */}
               <hr className="mb-4" />
-              <h4 className="mb-3">Payment</h4>
-              {/* <div className="block my-3">
-                <div className="custom-control custom-radio">
-                  <input
-                    id="credit"
-                    name="paymentMethod"
-                    type="radio"
-                    className="custom-control-input"
-                    checked
-                    required
-                  />
-                  <label className="custom-control-label" for="credit">
-                    Credit card
-                  </label>
-                </div>
-                <div className="custom-control custom-radio">
-                  <input
-                    id="debit"
-                    name="paymentMethod"
-                    type="radio"
-                    className="custom-control-input"
-                    required
-                  />
-                  <label className="custom-control-label" for="debit">
-                    Debit card
-                  </label>
-                </div>
-                <div className="custom-control custom-radio">
-                  <input
-                    id="paypal"
-                    name="paymentMethod"
-                    type="radio"
-                    className="custom-control-input"
-                    required
-                  />
-                  <label className="custom-control-label" for="paypal">
-                    Paypal
-                  </label>
-                </div>
-              </div> */}
-              <div className="block my-3 mb-[100px]">
-                <ul className="payment-methods">
-                  <li className="payment-method paypal">
+              <div className="my-3 mb-10">
+                <h4 className="mb-3 flex flex-row sm:flex-col items-center justify-center">
+                  Payment
+                </h4>
+                <ul className="payment-methods flex flex-col sm:flex-row items-center justify-center">
+                  <li className="payment-method paypal sm:w-full sm:mb-3 ml-[23px] mb-[10px]">
                     <input name="payment_methods" type="radio" id="paypal" />
                     <label htmlFor="paypal">PayPal</label>
                   </li>
 
-                  <li className="payment-method pagseguro">
+                  <li className="payment-method pagseguro sm:w-full sm:mb-3  mb-[10px]">
                     <input name="payment_methods" type="radio" id="pagseguro" />
                     <label htmlFor="pagseguro">PagSeguro</label>
                   </li>
 
-                  <li className="payment-method bankslip">
+                  <li className="payment-method bankslip sm:w-full sm:mb-3  mb-[10px]">
                     <input name="payment_methods" type="radio" id="bankslip" />
                     <label htmlFor="bankslip">Boleto</label>
                   </li>
@@ -286,6 +268,13 @@ const Checkout = () => {
           </div>
         </div>
       </div>
+    </div>
+  ) : (
+    <div className="flex justify-center">
+      <img
+        src={emty}
+        className="lg:h-[600px] lg:w-[800px] animate-pulse hover:skew-y-3 "
+      />
     </div>
   );
 };
