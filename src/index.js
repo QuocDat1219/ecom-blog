4d53da52c79837c96f481a1dba3f03c0f76d16d6
lenguyenquocdat1219@gmail.com <lenguyenquocdat1219@gmail.com>
2023-06-09 11:00:54 +0700

    newwwwwww

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@material-tailwind/react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-const root = ReactDOM.createRoot(document.getElementById("fb-root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
